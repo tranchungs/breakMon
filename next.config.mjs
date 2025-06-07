01a24cf66afd60f54fe3948fd07cc93dbe4bec11
tranchungs <tranchung2k@gmail.com>
2025-06-07 11:42:52 +0700

    HEADER

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "", // hoặc xóa hẳn phần headers để không gửi header này
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
